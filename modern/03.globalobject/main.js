fcae9ce3e4ffe44ec99bab942d900fbd00ca746c
Junpyeong Kim <junpyeong.kim@sk.com>
2015-09-13 23:08:42 +0900

    [modern] 3.4 exports 객체와

---
+++ modern/03.globalobject/main.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Junpyeong Kim on 2015. 9. 13..
+ *
+ * 3.4 exports 객체와
+ * - 모듈 == 자바스크립트
+ *
+ */
+
+var mymodule = require('./mymodule.js');
+//var mymodule = require('./mymodule');  // mymodule.js --> mymodule/index.js
+
+console.log('abs(-273) = %d', mymodule.abs(-273));
+console.log('circleArea(3) = %d', mymodule.circleArea(3));
---
+++ modern/03.globalobject/mymodule.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Junpyeong Kim on 2015. 9. 13..
+ *
+ * 3.4 exports 객체와
+ * - 모듈 == 자바스크립트 파일
+ */
+
+exports.abs = function (number) {
+    if (0 < number) {
+        return number;
+    } else {
+        return -number;
+    }
+};
+
+exports.circleArea = function (radius) {
+    return radius * radius * Math.PI;
+};
