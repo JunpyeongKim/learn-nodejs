e041e3174f7d9932b034a2c7eb3aa6a97ba1c73c
Junpyeong Kim <junpyeong.kim@sk.com>
2015-09-13 22:56:24 +0900

    [modern] 3.1

---
+++ modern/03.globalobject/global.var.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Junpyeong Kim on 2015. 9. 13..
+ *
+ * 3.1 전역 변수
+ *
+ * 웹 브라우저
+ * - 최상위 객체인 window 객체
+ * Node.js
+ * - global 객체, 전역 변수(__filename, __dirname), 전역 함수인 require()
+ * - 전역 객체 : console, exports, process, String, Number, Math
+ */
+
+console.log('__filename: ', __filename);
+console.log('__dirname: ', __dirname);
