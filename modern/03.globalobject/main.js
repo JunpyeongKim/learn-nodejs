e7cb3300da23140dd4403f7a1c7125287dfcbf0b
Junpyeong Kim <junpyeong.kim@sk.com>
2015-09-15 00:28:29 +0900

    [modern] 3.4 exports 객체와 모듈

--- modern/03.globalobject/main.js
+++ modern/03.globalobject/main.js
@@ -1,8 +1,10 @@
 /**
- * Created by Junpyeong Kim on 2015. 9. 13..
- *
  * 3.4 exports 객체와 모듈
- * - 모듈 == 자바스크립트
+ *
+ * Module == JavaScript File
+ *
+ * 모듈 생성 : exports 객체 사용
+ * 모듈 추출 : require() 함수 사용
  *
  */
 
