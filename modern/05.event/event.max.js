63e33afaf966d48207399f06fce7675afd049099
Junpyeong Kim <junpyeong.kim@sk.com>
2015-09-15 00:13:53 +0900

    [modern] 5.2 이벤트 연결 개수 제한

---
+++ modern/05.event/event.max.js
@@ -0,0 +1,26 @@
+/**
+ * 5.2 이벤트 연결 개수 제한
+ *
+ * setMaxListeners(limit)
+ *
+ */
+
+// turn on/off
+//process.setMaxListeners(15);
+//process.setMaxListeners(0);   // infinite
+
+// Default Max: 10 event listeners / 1 event
+process.on('exit', function () {});
+process.on('exit', function () {});
+process.on('exit', function () {});
+process.on('exit', function () {});
+process.on('exit', function () {});
+process.on('exit', function () {});
+process.on('exit', function () {});
+process.on('exit', function () {});
+process.on('exit', function () {});
+process.on('exit', function () {});
+process.on('exit', function () {});
+    // (node) warning: possible EventEmitter memory leak detected.
+    // 11 listeners added.
+    // Use emitter.setMaxListeners() to increase limit.
