676136bc9988fdc67d4599c690bae0f0c28dc438
Junpyeong Kim <junpyeong.kim@sk.com>
2015-09-15 00:40:15 +0900

    [modern] 3.3. process 객체

--- modern/03.globalobject/process.argv.js
+++ modern/03.globalobject/process.argv.js
@@ -1,18 +1,30 @@
 /**
- * Created by Junpyeong Kim on 2015. 9. 13..
- *
  * 3.3. process 객체
- * - 프로그램과 관련된
- * - argv, env, versi정보on, versions, arch, platform
+ *
+ * 프로그램과 관련된 정보를 나타내는 객체
+ * - argv, env, version, versions, arch, platform
  * - exit([exitCode=0]), memoryUsage(), uptime()
+ *
+ * $ node process.argv.js --exit 10000
+ *
  */
 
-// process.argv
-// $ node process.argv.js --exit 10000
+// attributes
+console.log('- process.env: ', process.env);
+console.log('- process.version: ', process.version);
+console.log('- process.versions: ', process.versions);
+console.log('- process.arch: ', process.arch);
+console.log('- process.platform: ', process.platform);
+
+// methods
+console.log('- process.memoryUsage(): ', process.memoryUsage());
+console.log('- process.uptime(): ', process.uptime());
+
+// argv & exit()
 process.argv.forEach(function (item, index) {
     console.log(index + ' : ' + typeof item + ' : ', item);
 
-    if (item == '--exit') {
+    if (item === '--exit') {
         var exitTime = Number(process.argv[index + 1]);
         setTimeout(function () {
             process.exit();
