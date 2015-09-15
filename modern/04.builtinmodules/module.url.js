026b6505959c6266630200cc7524291b3cf03ab9
Junpyeong Kim <junpyeong.kim@sk.com>
2015-09-16 00:33:08 +0900

    [modern] 4.2 url 모듈

---
+++ modern/04.builtinmodules/module.url.js
@@ -0,0 +1,16 @@
+/**
+ * 4.2 url 모듈
+ *
+ * parse(urlStr [, parseQueryString=false, slashesDenoteHost=false])
+ * - String --> Object
+ * format(urlObj)
+ * - Object --> String
+ * resolve(from, to)
+ * - 완전한 URL 문자열을 생성
+ *
+ */
+
+var url = require('url');
+
+var parsedObject = url.parse('http://hanb.co.kr/book/look.html?isbn=978-89-7914-874-9');
+console.log('url.parse(): ', parsedObject);
