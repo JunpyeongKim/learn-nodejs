4ebcc50624b2db644d27b6ecf88eb52abb288131
Junpyeong Kim <junpyeong.kim@sk.com>
2015-09-17 03:27:56 +0900

    [modern] 10.1 socket.io 모듈 기본

--- modern/10.socket.io/socket.io.server.js
+++ modern/10.socket.io/socket.io.server.js
@@ -4,6 +4,9 @@
  * - 10.1.2 웹 소켓 클라이언트
  * - 10.1.3 웹 소켓 이벤트
  *
+ * socket.io 모듈
+ * - WebSocket Server 를 쉽게 구현가능하게 해주는 모듈
+ *
  * $ npm install socket.io
  * Go to http://localhost:52273
  *
@@ -30,7 +33,7 @@ var server = http.createServer(function (request, response) {
 
 // port: 일반적으로 웹서버와 함께 사용하므로 server 객체를 매개 변수로 입력한다.
 var io = socketio.listen(server);
-//io.set('log level', 2);
+//io.set('log level', 2);  // not valid?
 io.sockets.on('connection', function (socket) {
     console.log('Socket Event: connection...');
 
