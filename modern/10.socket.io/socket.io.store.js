68094fee489656b1af58e8355b5e18ee7f0d8b26
Junpyeong Kim <junpyeong.kim@sk.com>
2015-09-17 03:02:58 +0900

    [modern] 10.3 클라이언트 정보 저장

--- modern/10.socket.io/socket.io.store.js
+++ modern/10.socket.io/socket.io.store.js
@@ -22,6 +22,7 @@ var server = http.createServer(function (request, response) {
 
 // port: 일반적으로 웹서버와 함께 사용하므로 server 객체를 매개 변수로 입력한다.
 var io = socketio.listen(server);
+var name = '';
 //io.set('log level', 2);
 io.sockets.on('connection', function (socket) {
     console.log('Socket Event: connection...');
@@ -29,11 +30,13 @@ io.sockets.on('connection', function (socket) {
     socket.on('setname', function (data) {
         console.log('Client Send Data:', data);
         //socket.set('name', data);
+        name = data;
     });
 
     socket.on('getname', function () {
         //socket.get('name', function (error, data) {
-        //    socket.emit('responsename', data);
+            data = name;
+            socket.emit('responsename', data);
         //});
     });
 });
