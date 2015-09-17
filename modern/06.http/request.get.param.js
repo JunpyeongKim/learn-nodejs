e4357444fa3a0e359590958fa29a116b0451454c
Junpyeong Kim <junpyeong.kim@gmail.com>
2015-09-17 13:32:10 +0900

    [modern] 6.4.3 GET 요청 매개 변수 추출

--- modern/06.http/request.get.param.js
+++ modern/06.http/request.get.param.js
@@ -19,7 +19,7 @@ var http = require('http');
 var url = require('url');
 
 http.createServer(function (request, response) {
-    var query = url.parse(request.url, true).query;
+    var query = url.parse(request.url, true).query; // {name:"rintiantta", region:"seoul"}
 
     response.writeHead(200, {'Content-Type': 'text/html'});
     response.end('<h1>' + JSON.stringify(query) + '</h1>');
