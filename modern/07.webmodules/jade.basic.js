ddc11d238ed85af435064c810dc7561a906ab209
Junpyeong Kim <junpyeong.kim@sk.com>
2015-09-20 16:03:24 +0900

    [modern] 7.2 Jade 모듈

---
+++ modern/07.webmodules/jade.basic.js
@@ -0,0 +1,32 @@
+/**
+ * 7.2 Jade 모듈
+ *
+ * $ npm install jade
+ *
+ * compiler(str)
+ * - Jade(HAML 파일 형식) --> HTML 변경하는 function을 생성
+ *
+ */
+
+var http = require('http');
+var fs = require('fs');
+var jade = require('jade');
+
+http.createServer(function (request, response) {
+    fs.readFile('JadePage.jade', 'utf8', function (error, data) {
+
+        if (error) {
+            console.log('Error:');
+        } else {
+            var fn = jade.compile(data);
+
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.end(fn({
+                name: 'RintIanTta',
+                description: 'Hello EJS With Node.js .. !'
+            }));
+        }
+    });
+}).listen(52273, function () {
+    console.log('Server Running at http://127.0.0.1:52273');
+});
