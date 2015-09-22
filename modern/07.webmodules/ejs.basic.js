bc520ecf4d463e4a9d0e18a2bfc9b3e8cb9ba7f4
Junpyeong Kim <junpyeong.kim@gmail.com>
2015-09-22 16:32:11 +0900

    [modern] 7.1 EJS 모듈

--- modern/07.webmodules/ejs.basic.js
+++ modern/07.webmodules/ejs.basic.js
@@ -11,6 +11,7 @@
  * - <%= VALUE %>
  *
  * https://github.com/visionmedia/ejs
+ *
  */
 
 var http = require('http');
@@ -19,11 +20,15 @@ var ejs = require('ejs');
 
 http.createServer(function (request, response) {
     fs.readFile('EJSPage.ejs', 'utf8', function (error, data) {
-        response.writeHead(200, {'Content-Type': 'text/html'});
-        response.end(ejs.render(data, {
-            name: 'RintIanTta',
-            description: 'Hello EJS With Node.js ...'
-        }));
+        if (error) {
+            console.log('Error: readFile(EJSPage.ejs),', error);
+        } else{
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.end(ejs.render(data, {
+                name: 'RintIanTta',
+                description: 'Hello EJS With Node.js ...'
+            }));
+        }
     });
 }).listen(52273, function () {
     console.log('Server Running at http://127.0.0.1:52273');
