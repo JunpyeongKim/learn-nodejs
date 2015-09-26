8fa486cc7adef31a88a9899c83bab605f97292fc
Junpyeong Kim <junpyeong.kim@sk.com>
2015-09-26 14:37:30 +0900

    [realtime] 6.웹 애플리케이션 개발 확장 모듈 - socket event - there are a few bugs

--- realtime/06.express-socket.io/bingo/public/javascripts/main.js
+++ realtime/06.express-socket.io/bingo/public/javascripts/main.js
@@ -8,23 +8,29 @@ var bingo = {
         this.is_my_turn = true;
 
         this.socket = io.connect();
+        //this.socket = io.connect('http://localhost:3000');
+
+
+        this.socket.on('connect', function (data) {
+            self.socket.emit('join', { username: $("#username").val() });
+        });
+
+
+        this.socket.on('update_users', function (data) {
+            self.update_userlist(data);
+        });
+
 
         this.socket.on('check_number', function (data) {
-            self.where_is_it(data_num);
+            self.where_is_it(data.num);
             self.print_msg(data.username + " checked '" + data.num + "'");
         });
 
         this.socket.on('game_started', function (data) {
-
+            self.print_msg(data.username + " started this game.");
+            $("#start_button").hide();
         });
 
-        this.socket.on('update_users', function (data) {
-
-        });
-
-        this.socket.on('connect', function (data) {
-
-        });
 
         var numbers = [];
         for (var i = 1; i <= 25; i++) {
@@ -32,7 +38,11 @@ var bingo = {
         }
 
         numbers.sort(function (a, b) {
-            //TODO:
+            var temp = parseInt(Math.random() * 10, 10);
+            var isOddOrEven = temp % 2;
+            var isPosOrNeg = temp > 5 ? 1 : -1;
+
+            return (isOddOrEven * isPosOrNeg)
         });
 
         $("table.bingo-board td").each(function (i) {
@@ -44,23 +54,67 @@ var bingo = {
         });
 
         $("#start_button").click(function () {
-            //TODO:
+            self.socket.emit('game_start', { username: $("#username").val() });
+            self.print_msg("You started this game.");
+            $("#start_button").hide();
         });
-    },
+    },  // end of init()
 
-    select_num: function () {},
+    select_num: function (obj) {
+        if (this.is_my_turn && !$(obj).attr("checked")) {
+            this.socket.emit('select', { username: $("#username").val(), num: $(obj).text() });
+            this.check_num(obj);
+            this.is_my_turn = false;
+        } else {
+            this.print_msg("it is not your turn");
+        }
+    },  // end of select_num()
 
-    where_is_it: function () {},
+    where_is_it: function (num) {
+        var self = this;
+        var obj = null;
 
-    check_num: function () {},
+        $("table.bingo-board td").each(function () {
+            if ($(this).text() === num ) {
+                self.check_num(this);
+            }
+        });
+    },  // end of where_is_it()
 
-    update_userlist: function () {},
+    check_num: function (obj) {
+        $(obj).css("text-decoration", 'line-through');
+        $(obj).css("color", "#ccc");
+        $(obj).attr("checked", true);
+    },  // end of check_num()
+
+    update_userlist: function (data) {
+        var self = this;
+
+        $("#list").empty();
+
+        console.log(data);
+
+        $.each(data, function (key, value) {
+            var turn = "(-)&nbsp;";
+
+            if (value.turn === true) {
+                turn = "(*)&nbsp;";
+                console.log(value.name);
+                console.log($("#username").val());
+                if (value.name === $("#username").val()) {
+                    self.is_my_turn = true;
+                }
+            }
+
+            $("#list").append(turn + value.name + "<br/>");
+        });
+    },  // end of update_userlist()
 
     leave: function () {},
 
     print_msg: function (msg) {
         $("#logs").append(msg + "<br/>");
-    }
+    }   // end of print_msg()
 };
 
 $(document).ready(function () {
--- realtime/README.md
+++ realtime/README.md
@@ -1,2 +1,3 @@
 # 실시간 웹 애플리케이션 개발을 위한 Node.js 프로그래밍
 - 류성태, 프리렉
+- http://freelec.co.kr/bbs/list_book_pds.asp --> Node.js 프로그래밍
