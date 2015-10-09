8ac0700eaf796785a640be9f34a7804bb2d27702
Junpyeong Kim <junpyeong.kim@sk.com>
2015-10-09 15:27:29 +0900

    [express-webapp] 2. Your First Express App - view

--- express-webapp/02.firstapp/express-app/app.js
+++ express-webapp/02.firstapp/express-app/app.js
@@ -2,15 +2,25 @@ var http = require('http');
 var express = require('express');
 var app = express();
 
-http.createServer(app).listen(3000, function () {
-    console.log('Express app started');
-});
+app.set('view engine', 'jade');
+app.set('views', './views');
 
 // 404 & 500 에러처리 - router 미들웨어에 구현되어 있다
 
-// 서버를 시작시키는 고드 다음에 라우트가 정의되어 있어도 정상 동작하는 이유
-//  - 라우트가 참조값 형태로 전달되는 app 객체에 정의되기 때문
+// homepage
 app.get('/', function (req, res) {
-    //res.send('Welcome!');
-    res.send('<h1>Welcome!</h1>');
+    res.render('index');
+});
+
+//say-hello
+app.get('/say-hello', function (req, res) {
+    res.render('hello');
+});
+
+app.get('/test', function (req, res) {
+    res.send('this is a test');
+});
+
+http.createServer(app).listen(3000, function () {
+    console.log('App started');
 });
